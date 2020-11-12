ae3392bfe30b8f37882ccddbf63b619024065159
Sina <stavakkoli1999@gmail.com>
2020-11-13 00:16:27 +0330

    pandas can't be imported. VA!

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
-import pandas
+# import pandas as pd
+import xml.etree.ElementTree as xee
 
 
-def load_en():
-    document = pandas.read_csv()
+# def load_en():
+#     document = pandas.read_csv('ted_talks.csv')
+#     return document
+
+
+def load_pr():
+    url = '{http://www.mediawiki.org/xml/export-0.10/}'
+    root = xee.parse('./data/Persian.xml').getroot()
+    # TODO: is this the suitable xml format?
+    collection = [page.find(f'{url}revision/{url}text').text for page in root.findall(url + 'page')]
+    titles = [page.find(f'{url}title').text for page in root.findall(url + 'page')]
+    ids = [int(page.find(f'{url}id').text) for page in root.findall(url + 'page')]
+
+    return collection, titles, ids
+
+
+# print("pee pee")
+op1 = int(input("Please select the language of the document:\n1. English\t2. Persian\n"))
+while True:
+    if op1 == 1:
+        lang = 'english'
+        break
+    elif op1 == 2:
+        lang = 'persian'
+        break
+    else:
+        print("please enter a valid option (1 or 2)")
+        op1 = int(input("1. English\t2. Persian\n"))
+
+k_w = input("Please enter k & w:\n").split()
+k, w = int(k_w[0]), int(k_w[1])
+
+query = input("Now enter your query:\n")
+
+print("language: ", lang, "\nk: ", k, "\nw: ", w, "\nquery: ", query)
