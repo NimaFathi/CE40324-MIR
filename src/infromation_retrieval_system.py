8bd83c0ec1913863e754980b7ee07f90eea439db
NimaFathi <nimafathinimafathi@gmail.com>
2020-11-13 19:41:46 +0330

    update information_retrieval_system.py and add tf_idf.py

--- src/infromation_retrieval_system.py
+++ src/infromation_retrieval_system.py
@@ -1,6 +1,8 @@
 from src.preprocess import PreProcess, PreProcessEnglish, PreProcessPersian
 import pandas as pd
 import xml.etree.ElementTree as ET
+from .PositionalIndex import PositionalIndex
+from .BigramIndex import BiGramIndex
 
 
 class IRSystem:
@@ -14,13 +16,51 @@ class IRSystem:
         self.body_documents = []
         self.title_documents = []
         self.ids = None
+        self.positional_index_body = None
+        self.positional_index_title = None
+        self.bigram_index_body = None
+        self.bigram_index_title = None
 
-        if ir_type == self.TYPE_PERSIAN:
+        if self.type == self.TYPE_PERSIAN:
             self.persian_system()
-        elif ir_type == self.TYPE_ENGLISH:
+        elif self.type == self.TYPE_ENGLISH:
             self.english_system()
-        elif ir_type not in self.TYPES:
+        elif self.type not in self.TYPES:
             raise ValueError('lang should be "english" or "persian"')
+        self.initiate_indices()
+
+    def initiate_indices(self):
+        positional_index_names = self.create_index_names('positional-index', self.type)
+        bigram_index_names = self.create_index_names('bigram-index', self.type)
+
+        self.positional_index_body = PositionalIndex(
+            name=positional_index_names['body'],
+            docs=self.body_documents,
+            ids=self.ids
+        )
+        self.positional_index_title = PositionalIndex(
+            name=positional_index_names['title'],
+            docs=self.title_documents,
+            ids=self.ids
+        )
+
+        self.bigram_index_body = BiGramIndex(
+            name=bigram_index_names['body'],
+            docs=self.body_documents,
+            ids=self.ids
+        )
+        self.bigram_index_title = BiGramIndex(
+            name=bigram_index_names['title'],
+            docs=self.title_documents,
+            ids=self.ids
+        )
+
+    @staticmethod
+    def create_index_names(index_type, lang_type):
+        return {
+            doc_type: index_type + '-' + doc_type + '-' + lang_type
+            for doc_type in ['title', 'body']
+        }
 
     def persian_system(self):
         tree = ET.parse('../data/Persian.xml')
---
+++ src/tf_idf.py
@@ -0,0 +1,16 @@
+class TfIdfSearch:
+    def __init__(self, query, documents):
+        self.query = query
+        self.documents = documents
+
+    def tf_idf_score(self):
+        pass
+
+    def idf(self):
+        pass
+
+    def tf(self):
+        pass
+
+    def norm(self):
+        pass
