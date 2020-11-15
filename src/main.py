e2b7b262146ddcf63d41c0330fbb4212365a7be9
Sina <stavakkoli1999@gmail.com>
2020-11-15 08:45:42 +0330

    connected api

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# from src.infromation_retrieval_system import IRSystem
+from src.infromation_retrieval_system import IRSystem
 from src.QueryCorrection import correct_query
 
 
@@ -28,20 +28,34 @@ while True:
         print("please enter a valid option (1 or 2)")
         op2 = int(input("1. information retrieval\n2. query correction\n"))
 
-# if flag1:
-#     ir = IRSystem(lang)
-#     query = input("\nnow enter your query:\n")
-#     wanted_outcomes = int(input("\nenter the number of outcomes you want:"))
-#     temp = int(input("\nwhat retrieve type you want to use?\n1. body\t2. title\n"))
-#     if temp == 1:
-#         retrieve_type = "body"
-#     else:
-#         retrieve_type = "title"
-#     ir.retrieve_query_answer(query, wanted_outcomes, retrieve_type)
+query = 0
+while True:
+    if flag1:
+        ir = IRSystem(lang)
+        query = input("\nnow enter your query:\n")
+        wanted_outcomes = int(input("\nenter the number of outcomes you want:"))
+        temp = int(input("\nwhat retrieve type you want to use?\n1. body\t2. title\n"))
+        if temp == 1:
+            retrieve_type = "body"
+        else:
+            retrieve_type = "title"
+        ir.retrieve_query_answer(query, wanted_outcomes, retrieve_type)
+        con = int(input("do you want to use query correction system? 1. yes 2. no\n"))
+        if con == 1:
+            flag2 = True
+        else:
+            break
 
-if flag2:
-    query = input("enter your misspelled query:\n")
-    dictionary = input("now enter a dictionary containing correct form of your query's words "
-                       "(and maybe other words):\n")
-    print("corrected query is:\n")
-    print(correct_query(query, dictionary.split()))
+    if flag2:
+        if query == 0:
+            query = input("enter your misspelled query:\n")
+        else:
+            new_query = int(input("do you want to use a new query or the last one?\n1. new 2. previous\n"))
+            if new_query == 1:
+                query = input("enter your misspelled query:\n")
+
+        dictionary = input("now enter a dictionary containing correct form of your query's words "
+                           "(and maybe other words):\n")
+        print("corrected query is:\n")
+        print(correct_query(query, dictionary.split()))
+        break
