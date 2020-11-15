b3b86cb794c32409e01f191dae3667878804b70b
NimaFathi <nimafathinimafathi@gmail.com>
2020-11-15 04:32:25 +0330

    correct a typo error

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .BigramIndex import BiGramIndex
 from .Compressor import Compressor
-from .infromation_retrieval_system import IRSystem
+from .information_retrieval_system import IRSystem
 from .PositionalIndex import PositionalIndex
 from .preprocess import PreProcessEnglish, PreProcessPersian, PreProcess
