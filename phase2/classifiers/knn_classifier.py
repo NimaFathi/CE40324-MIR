1c195447571c14a17d489f58a0a77bafe8610f40
Sina <stavakkoli1999@gmail.com>
2020-12-19 22:37:58 +0330

    TEMP COMMIT

--- phase2/classifiers/knn_classifier.py
+++ phase2/classifiers/knn_classifier.py
@@ -1,38 +1,33 @@
-from .base_classifier import BaseClassifier
-
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 
-class KNNClassifier(BaseClassifier):
-    def __init__(self, k, x_train, y_train):
-        super().__init__(x_train, y_train, classifier=None)
+class KNNClassifier:
+    def __init__(self, k):
         self.k = k
-        self.standard_scaler = StandardScaler()
-        self.x = None
-        self.y = None
+        self.X_train = None
+        self.y_train = None
+        self.scaler = StandardScaler()
 
-    def fit(self):
-        self.standard_scaler.fit(self.x_train)
-        # use fit_transform on training data
-        self.x = self.standard_scaler.fit_transform(self.x_train)
-        self.y = self.y_train
+    def fit(self, X, y):
+        self.scaler.fit(X)
+        self.X_train = self.scaler.transform(X)
+        self.y_train = y
 
-    def get_neighbors(self, item):
-        train = []
-        for i in range(self.x.shape[0]):
-            train.append((self.y[i], abs(np.linalg.norm(item - self.x[i], 2))))
-        train.sort(key=lambda t: t[1])
-        neighbors = []
-        for i in range(self.k):
-            neighbors.append(train[i][0])
-        return neighbors
+    @staticmethod
+    def majority(lst):
+        return max(set(lst), key=lst.count)
 
-    def predict(self, test):
-        # use transform on test data
-        x_test = self.standard_scaler.transform(test)
-        y_predict = []
-        for data in x_test:
-            neighbors = self.get_neighbors(data)
-            y_predict.append(max(set(neighbors), key=neighbors.count))
-        return np.array(y_predict)
+    @staticmethod
+    def distance(v1, v2):
+        return abs(np.linalg.norm(v2 - v1, 2))
+
+    def neighbors(self, x):
+        d = [(self.y_train[i], self.distance(self.X_train[i], x)) for i in range(self.X_train.shape[0])]
+        d.sort(key=lambda t: t[1])
+        return [d[i][0] for i in range(self.k)]
+
+    def predict(self, X):
+        X_test = self.scaler.transform(X)
+        y_pred = [self.majority(self.neighbors(x)) for x in X_test]
+        return np.array(y_pred)
