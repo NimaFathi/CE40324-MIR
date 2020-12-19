2fff26ad6ba75c6aeb2f76422f589393f1e7ba05
Niwgoat <nimafathinimafathi@gmail.com>
2020-12-20 01:00:17 +0330

    fix knn

--- phase2/classifiers/knn_classifier.py
+++ phase2/classifiers/knn_classifier.py
@@ -8,14 +8,11 @@ class KNNClassifier(BaseClassifier):
     def __init__(self, k, x_train, y_train):
         super().__init__(x_train, y_train, classifier=None)
         self.k = k
-        self.standard_scaler = StandardScaler()
         self.x = None
         self.y = None
 
     def fit(self):
-        self.standard_scaler.fit(self.x_train)
-        # use fit_transform on training data
-        self.x = self.standard_scaler.fit_transform(self.x_train)
+        self.x = self.x_train
         self.y = self.y_train
 
     def get_neighbors(self, item):
@@ -30,7 +27,7 @@ class KNNClassifier(BaseClassifier):
 
     def predict(self, test):
         # use transform on test data
-        x_test = self.standard_scaler.transform(test)
+        x_test = test
         y_predict = []
         for data in x_test:
             neighbors = self.get_neighbors(data)
