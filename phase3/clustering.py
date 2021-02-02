f76821a6d44483ecac48e70c15e3e36e74193b0f
ellykhodaie <elahekhodaie@yahoo.com>
2021-02-02 23:32:01 +0330

    the csv creater function added

--- phase3/clustering.py
+++ phase3/clustering.py
@@ -41,3 +41,28 @@ def GMM(data, vectors, n_components=None, **kwargs):
     model.fit(vectors)
     sizes = model.predict_proba(vectors)
     return model.predict(vectors), sizes
+
+
+
+# cluster type is one of the above three algorithm types 
+
+ def return_clustered_csv(data, cluster_type, tfidf = None, w2v=None, options=None, save=False):
+
+    options = options or dict()
+    result = pd.DataFrame({'link': data['link']})
+    name = f'{cluster_type.__name__}' + ('' if not options else (
+            ' (' + ','.join(f'{i}={j}' for i, j in options.items()) + ')'))
+    if tfidf is not None:
+        result['tf-idf'], sizes = cluster_type(data, tfidf, **options)
+        plot2d(tfidf, result['tf-idf'], true_labels=data['major_cls'], sizes=sizes, title=f'{name} [tf-idf]')
+
+    if w2v is not None:
+        result['w2v'], sizes = cluster_type(data, w2v, **options)
+        plot2d(w2v, result['w2v'], true_labels=data['major_cls'], sizes=sizes, title=f'{name} [w2v]')
+
+    if save:
+        result[['link', 'tf-idf']].rename(columns={'link': 'link', 'tf-idf': 'pred'}).to_csv(
+            f'outputs/{cluster_type.__name__}-tfidf.csv')
+        result[['link', 'w2v']].rename(columns={'link': 'link', 'w2v': 'pred'}).to_csv(
+            f'outputs/{cluster_type.__name__}-w2v.csv')
+    return result   
