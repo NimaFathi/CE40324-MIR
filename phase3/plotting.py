bfaca4d6e7b4b2fad91faf7a61af39f61e4245e2
Sina <stavakkoli1999@gmail.com>
2021-02-04 13:49:20 +0330

    pca added to plotting

--- phase3/plotting.py
+++ phase3/plotting.py
@@ -1,15 +1,19 @@
 from yellowbrick.cluster import kelbow_visualizer, intercluster_distance
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
-from .preprocess import *
+# from .preprocess import *
 from .clustering import return_clustered_csv, kmeans, load_data, vectorize, get_res
 
+# loading data
 data, major_labels, minor_labels = load_data(stem=False, lemmatize=True, remove_conjunctions=True)
 tfidf, w2v = vectorize(data, w2v_options=dict(size=64, iter=128, min_count=2))
-pca_tfidf = pca(64, tfidf.toarray(), random_state=666)
+pca_tfidf = PCA(64, random_state=666).fit_transform(tfidf.toarray())
 
+# cluster diagrams
 get_res(return_clustered_csv(data, kmeans, pca_tfidf, w2v, options=dict(random_state=666), save=False), data=data)
 
+# elbow charts and Inter-cluster Distance Maps
 print("TF-IDF:\n")
 _ = kelbow_visualizer(KMeans(random_state=666), pca_tfidf, k=(2, 16), metric='silhouette')
 _ = intercluster_distance(KMeans(n_clusters=5, random_state=666), pca_tfidf)
