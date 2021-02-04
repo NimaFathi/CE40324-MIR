4887f3ee68eca8427477ea2522ee80c86caa417a
Sina <stavakkoli1999@gmail.com>
2021-02-04 13:33:27 +0330

    load in plotting

--- phase3/plotting.py
+++ phase3/plotting.py
@@ -2,7 +2,11 @@ from yellowbrick.cluster import kelbow_visualizer, intercluster_distance
 from sklearn.cluster import KMeans
 
 from .preprocess import *
-from .clustering import return_clustered_csv, kmeans
+from .clustering import return_clustered_csv, kmeans, load_data, vectorize
+
+data, major_labels, minor_labels = load_data(stem=False, lemmatize=True, remove_conjunctions=True)
+tfidf, w2v = vectorize(data, w2v_options=dict(size=64, iter=128, min_count=2))
+pca_tfidf = pca(64, tfidf.toarray(), random_state=666)
 
 print("TF-IDF:\n")
 _ = kelbow_visualizer(KMeans(random_state=666), pca_tfidf, k=(2, 16), metric='silhouette')
