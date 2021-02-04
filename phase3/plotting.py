47736b87d26413855a8c2c6abcc5ec283e4d94d9
Sina <stavakkoli1999@gmail.com>
2021-02-04 13:45:38 +0330

    kmeans cluster diagram added

--- phase3/plotting.py
+++ phase3/plotting.py
@@ -2,15 +2,13 @@ from yellowbrick.cluster import kelbow_visualizer, intercluster_distance
 from sklearn.cluster import KMeans
 
 from .preprocess import *
-from .clustering import return_clustered_csv, kmeans, load_data, vectorize
+from .clustering import return_clustered_csv, kmeans, load_data, vectorize, get_res
 
 data, major_labels, minor_labels = load_data(stem=False, lemmatize=True, remove_conjunctions=True)
 tfidf, w2v = vectorize(data, w2v_options=dict(size=64, iter=128, min_count=2))
 pca_tfidf = pca(64, tfidf.toarray(), random_state=666)
 
-unsup_kmeans_res = return_clustered_csv(data, kmeans, pca_tfidf, w2v,
-                                 options=dict(random_state=666),
-                                 save=False)
+get_res(return_clustered_csv(data, kmeans, pca_tfidf, w2v, options=dict(random_state=666), save=False), data=data)
 
 print("TF-IDF:\n")
 _ = kelbow_visualizer(KMeans(random_state=666), pca_tfidf, k=(2, 16), metric='silhouette')
