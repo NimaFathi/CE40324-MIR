bda1c0a95339fb49ba42b4455c15ba981b6fcf73
Sina <stavakkoli1999@gmail.com>
2021-02-05 19:37:46 +0330

    page rank

--- phase3/page_rank.py
+++ phase3/page_rank.py
@@ -4,36 +4,41 @@ import numpy as np
 import json
 
 
-def page_rank(alpha: float = 0.1, convergence_limit: float = 1, top_count: int = 10):
+def page_rank(file_name, alpha: float = 0.1, convergence_limit: float = 1, top_count: int = 10):
+    data = pd.read_json(file_name)
+    scores = get_eigen_vector(create_matrix(data, alpha), conv_limit)
+    ranks = np.argsort(-scores.reshape(-1))
+    data['page-rank'] = ranks
+    return data.iloc[np.argsort(ranks)]
     # creating matrix
-    nodes = {}
-    ids = list(nodes.keys())
-    size = len(ids)
-    vec = []
-    for i in nodes:
-        interior = []
-        neighbor = i.references
-        for j in range(size):
-            cap = len(neighbor)
-            if ids[j] in neighbor:
-                interior.append((1 - alpha) / cap)
-            else:
-                interior.append(alpha / (size - cap))
-        vec.append(interior)
-    matrix = np.matrix(vec).T
-    # matrix created
-    # getting vector res
-    res = np.random.rand(matrix.shape[1], 1)
-    res = res / np.linalg.norm(res, 1)
-    distance = convergence_limit + 10
-    while distance > convergence_limit:
-        prev_mat = res
-        res = matrix @ res
-        distance = np.linalg.norm(res - prev_mat)
-    vector = list(res.T)
-    sorted_vec = list(sorted(vector, reverse=True))
-    best = []
-    ids = list(nodes.keys())
-    for i in range(top_count):
-        best.append(nodes[ids[vector.index(sorted_vec[i])]].title)
-    return best
+    # nodes = {}
+    # ids = list(nodes.keys())
+    # size = len(ids)
+    # vec = []
+    # for i in nodes:
+    #     interior = []
+    #     neighbor = i.references
+    #     for j in range(size):
+    #         cap = len(neighbor)
+    #         if ids[j] in neighbor:
+    #             interior.append((1 - alpha) / cap)
+    #         else:
+    #             interior.append(alpha / (size - cap))
+    #     vec.append(interior)
+    # matrix = np.matrix(vec).T
+    # # matrix created
+    # # getting vector res
+    # res = np.random.rand(matrix.shape[1], 1)
+    # res = res / np.linalg.norm(res, 1)
+    # distance = convergence_limit + 10
+    # while distance > convergence_limit:
+    #     prev_mat = res
+    #     res = matrix @ res
+    #     distance = np.linalg.norm(res - prev_mat)
+    # vector = list(res.T)
+    # sorted_vec = list(sorted(vector, reverse=True))
+    # best = []
+    # ids = list(nodes.keys())
+    # for i in range(top_count):
+    #     best.append(nodes[ids[vector.index(sorted_vec[i])]].title)
+    # return best
