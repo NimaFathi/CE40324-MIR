b090c21e880493da2d735f019eb7bd92a417436c
Niwgoat <nimafathinimafathi@gmail.com>
2021-02-05 21:03:00 +0330

    last commit -- full phase3

--- phase3/page_rank.py
+++ phase3/page_rank.py
@@ -1,11 +1,12 @@
 from os import listdir
-
+import pandas as pd
 import numpy as np
 import json
 
 
 def page_rank(file_name, convergence_limit: float = 1):
     data = pd.read_json(file_name)
+    print(data)
     matrix = np.zeros((len(data), len(data)))
     alpha = 0.1
     for index in range(len(data)):
@@ -25,35 +26,8 @@ def page_rank(file_name, convergence_limit: float = 1):
     ranks = np.argsort(-scores.reshape(-1))
     data['page-rank'] = ranks
     return data.iloc[np.argsort(ranks)]
-    # creating matrix
-    # nodes = {}
-    # ids = list(nodes.keys())
-    # size = len(ids)
-    # vec = []
-    # for i in nodes:
-    #     interior = []
-    #     neighbor = i.references
-    #     for j in range(size):
-    #         cap = len(neighbor)
-    #         if ids[j] in neighbor:
-    #             interior.append((1 - alpha) / cap)
-    #         else:
-    #             interior.append(alpha / (size - cap))
-    #     vec.append(interior)
-    # matrix = np.matrix(vec).T
-    # # matrix created
-    # # getting vector res
-    # res = np.random.rand(matrix.shape[1], 1)
-    # res = res / np.linalg.norm(res, 1)
-    # distance = convergence_limit + 10
-    # while distance > convergence_limit:
-    #     prev_mat = res
-    #     res = matrix @ res
-    #     distance = np.linalg.norm(res - prev_mat)
-    # vector = list(res.T)
-    # sorted_vec = list(sorted(vector, reverse=True))
-    # best = []
-    # ids = list(nodes.keys())
-    # for i in range(top_count):
-    #     best.append(nodes[ids[vector.index(sorted_vec[i])]].title)
-    # return best
+
+
+if __name__ == '__main__':
+    x = page_rank('./crawled_papers.json')
+    print(x)
